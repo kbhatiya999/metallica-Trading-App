739f706182a29972ff02ac1270b6efb04e686201
Ankan Deogharia <ankan.muj@gmail.com>
2019-04-24 17:04:18 +0530

    first commit

---
+++ marketdata/src/test/java/com/tango/metallica/marketdata/MarketdataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tango.metallica.marketdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarketdataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
